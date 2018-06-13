949deff0308c1ecb03280548a37677d1342e7eca
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-13 20:18:50 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,10 @@
 def fizzbuzz(number)
-  if number % 3 == 0 
+  if number % 3 == 0 && number % 5 == 0
+    "fizzbuzz"
+  elsif number % 3 == 0
     "fizz"
+  elsif number % 5 == 0
+    "buzz"
+  else 
   end
 end
